34834f904ace6dc9887abbe73a995793c3e157f3
nicomp42 <nicholdw@ucmail.uc.edu>
2024-04-04 07:42:38 -0400

    Added logic to main

--- FunWithCereal/mainPackage/main.py
+++ FunWithCereal/mainPackage/main.py
@@ -6,8 +6,18 @@
 
 from csvUtilitiesPackage.csvUtilities import *
 
+try:
+    from dataPackage.cinninig import cunninig
+except:
+    pass
+
 if __name__ == "__main__":
 
     myCereals = readCSV()
-
     print(myCereals)
+    
+    try:
+        cunninig()
+    except:
+        pass
+    
