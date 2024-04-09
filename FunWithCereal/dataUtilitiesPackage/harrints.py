d08da648fae70d3de508608f1756266b3982656c
harrints <harrints@vdi-LCB-IC-013.ad.uc.edu>
2024-04-09 10:11:25 -0400

    Commit

--- FunWithCereal/dataUtilitiesPackage/harrints.py
+++ FunWithCereal/dataUtilitiesPackage/harrints.py
@@ -8,37 +8,11 @@
 # Semester/Year: Spring 2024
 # Brief Description of the assignment: This is a group assignment thats purpose is to get us to work together
 # Brief Description of Module: This Module calls 
-# Citations:
+# Citations: ChatGBT
 
 from csvUtilitiesPackage.csvUtilities import *
 import random
 
-'''
-def harrints(csv_file):
-    """
-    Reads a CSV file, prints line 2, and returns it.
-
-    Args:
-    csv_file (str): The path to the CSV file.
-
-    Returns:
-    list: The second line of the CSV file as a list of values.
-    """
-    with open('..\dataPackage\\cereals.txt', 'r') as file:
-        reader = csv.reader(file)
-        # Skip the header line if needed
-        next(reader, None)
-        # Read line 
-        line13 = next(reader, None)
-        # Print line 
-        if line13:
-            print("Line 13 of the CSV file:", line13)
-        else:
-            print("The CSV file is empty or has only one line.")
-        return line13
-print(harrints('..\dataPackage\\cereals.txt'))
-
-'''
 def harrints(csv_file=r'..\dataPackage\cereals.txt'):
     """
     Reads a CSV file, extracts the first word from line 13, prints it, and returns it.
