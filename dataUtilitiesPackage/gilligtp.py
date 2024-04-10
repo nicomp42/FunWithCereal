64078455cb089896e9be32f9c299aebd1da3e7ae
pgill <pgill@Scrub>
2024-04-10 02:16:52 -0400

    second commit, switched to fiber after sugar struggle

--- dataUtilitiesPackage/gilligtp.py
+++ dataUtilitiesPackage/gilligtp.py
@@ -1,23 +1,41 @@
 #gilligtp.py
-
+'''
+# Name: Tom Gilligan
+# email: gilligtp@mail.uc.edu
+# Assignment Number: Assignment 10
+# Due Date: 4/11/24
+# Course/Section: 002
+# Semester/Year: Spring 24
+# Brief Description of the assignment: read csv data and push
+# Citations: w3schools, the google, stackoverflow
+# Anything else that's relevant: n/a
+'''
 import csv
 
 def gilligtp():
+    
+    
     """
-    Display the cereals with the most sugar
-    @return: sugar cereal
+    Finds and prints information about the cereal with the most fiber per gram.
+    @return: fiberiest_cereal
     """
+
+    max_fiber = 0
+    fiberiest_cereal = None
+
     with open('..\dataPackage\\cereals.txt') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter='\t')
-        line_count = 0
-        cereals = []
+        next(csv_reader)  # Skip the header row 
+
         for row in csv_reader:
-            if line_count == 0:
-                #print(f'Column names are {", ".join(row)}')
-                line_count += 1
-            else:
-                cereals.append(row)
-                #print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-                line_count += 1
-        #print(cereals)
-    return cereals
+            cereal_name = row[0]
+            fiber = float(row[10])
+
+            if fiber > max_fiber:
+                max_fiber = fiber
+                fiberiest_cereal = cereal_name
+
+    if fiberiest_cereal:
+        print(f"This cereal has the most fiber per gram: {fiberiest_cereal}") 
+
+gilligtp()
